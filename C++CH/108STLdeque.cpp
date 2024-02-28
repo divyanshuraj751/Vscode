b5d6e0f9bde7b8127cbc1294a61bf78df1850f6c
divyanshuraj751 <146466712+divyanshuraj751@users.noreply.github.com>
2024-02-28 21:37:26 +0530

    up

--- C++CH/108STLdeque.cpp
+++ C++CH/108STLdeque.cpp
@@ -8,20 +8,23 @@ int main() {
 
    for(int i:d) {
     cout<<i<<" ";
-   }cout<<endl;
+   }
+   cout<<endl;
+   
    d.pop_back();
+   
    for(int i:d) {
     cout<<i<<" ";
-   }cout<<endl;
+   }
+   
+   cout<<endl;
+   
    d.push_back(1);
+   
    d.pop_front();
+   
    for(int i:d){
     cout<<i<<" ";
-   }cout<<endl;
+   }
 
-   cout<<"Print first index element "<<d.at(1)<<endl;
-
-   cout<<"Front "<<d.front()<<endl;
-   cout<<"back "<<d.back()<<endl;
-   
 }
