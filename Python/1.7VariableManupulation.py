74c975bb7916fcd287efe12b8c70d8f57bd58488
divyanshuraj751 <146466712+divyanshuraj751@users.noreply.github.com>
2024-03-02 02:43:18 +0530

    02-03-24

---
+++ Python/1.7VariableManupulation.py
@@ -0,0 +1,9 @@
+a = input("a:")
+b = input("b:")
+
+t = a
+a = b
+b = t 
+
+print("a:"+a)
+print("b:"+b)
---
+++ Python/1.8Project.py
@@ -0,0 +1,4 @@
+print("Welcome To Band name Generator\n")
+city = input("city name\n")
+pet = input("pet name\n")
+print(city+" "+pet)
