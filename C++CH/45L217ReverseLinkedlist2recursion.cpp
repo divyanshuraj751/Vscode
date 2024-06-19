6f071b15c73361e9e1c7e51b98cc76d7f04d6b98
Divyanshu Raj <146466712+divyanshuraj751@users.noreply.github.com>
2024-06-19 11:55:23 +0530

    Add files via upload

---
+++ C++CH/45L217ReverseLinkedlist2recursion.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+    int data;
+    Node *next;
+    Node(int data) {
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+void reverse(Node* &head,Node* curr,Node* prev) {
+    //base case
+    if(curr == NULL) {
+        head = prev;
+        return;
+    }
+    Node* forward = curr -> next;
+    reverse(head,forward,curr);
+    curr -> next = prev;
+}
+
+Node* reverseLinkedList(Node *head) {
+    Node* curr = head;
+    Node* prev = NULL;
+    reverse(head,curr,prev);
+    return head;
+}
+
+void printLinkedList(Node *head) {
+    Node *temp = head;
+    while(temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void insertAtHead(Node* &head,int d) {
+    //new node create
+    Node* temp = new Node(d);
+    temp -> next = head;
+    head = temp;
+}
+
+void insertAtTail(Node* &tail,int d) {
+    //new node create
+    Node* temp = new Node(d);
+    tail -> next = temp;
+    tail = temp;
+}
+
+void insertAtPosition(Node* &tail, Node* &head, int position, int d) {
+    //insert at start
+    if(position == 1) {
+        insertAtHead(head,d);
+        return;
+    }
+    Node* temp = head;
+    int cnt = 1;
+
+    while(cnt < position-1) {
+        temp = temp -> next;
+        cnt++;
+    }
+    //inserting at last position
+    if(temp -> next == NULL) {
+        insertAtTail(tail,d);
+        return;
+    }
+    //creating a node for d
+    Node* nodeToInsert = new Node(d);
+    nodeToInsert -> next = temp -> next;
+    temp -> next = nodeToInsert;
+}
+
+int main() {
+    Node *node1 = new Node(1);
+    Node* head = node1;
+    Node* tail = node1;
+    
+    insertAtPosition(tail,head,2,20);
+    insertAtPosition(tail,head,3,98);
+    insertAtPosition(tail,head,4,73);
+    insertAtPosition(tail,head,5,43);
+    insertAtPosition(tail,head,6,53);
+    insertAtPosition(tail,head,7,63);
+    insertAtPosition(tail,head,8,3);
+
+    cout << "Original Linked List: ";
+    printLinkedList(head);
+
+    Node *reversedHead = reverseLinkedList(head);
+
+    cout << "Reversed Linked List: ";
+    printLinkedList(reversedHead);
+
+    return 0;
+}
+
+
+//tc - 0(n)
+//sc - 0(n)
