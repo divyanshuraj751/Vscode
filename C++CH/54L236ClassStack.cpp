12547622d5f87a19cb32994bfe32ffcafa8c2ab3
Divyanshu Raj <146466712+divyanshuraj751@users.noreply.github.com>
2024-06-28 00:02:25 +0530

    Add files via upload

---
+++ C++CH/54L236ClassStack.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+using namespace std;
+
+class Stack {
+    //properties
+    public:
+        int *arr;
+        int top;
+        int size;
+    
+    //behaviour
+    Stack(int size) {
+        this -> size = size;
+        arr = new int(size);
+        top = -1;
+    }
+
+    void push(int element) {
+        if(size - top > 1) {
+            top++;
+            arr[top] = element;
+        }
+        else {
+            cout<<"Stack Overflow "<<endl;
+        }
+    }
+
+    void pop() {
+        if(top>=0) {
+            top--;
+        }
+        else {
+            cout<<"Stack Underflow "<<endl;
+        }
+    }
+
+    int peek() {
+        if(top >= 0) {
+        return arr[top];
+        }
+        else {
+            cout<<"Stack is empty "<<endl;
+            return -1;
+        }
+    }
+
+    bool isEmpty() {
+        if(top == -1) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+int main() {
+    Stack st(5);
+
+    st.push(5);
+    st.push(32);
+    st.push(12);
+
+    cout<<st.peek()<<endl;
+
+    st.pop();
+    cout<<st.peek()<<endl;
+
+    st.pop();
+    cout<<st.peek()<<endl;
+
+    st.pop();
+    cout<<st.peek()<<endl;
+
+    if(st.isEmpty()) {
+        cout<<"Stack is empty "<<endl;
+    }
+    else {
+        cout<<"Stack is not empty "<<endl;
+    }
+
+    st.push(88);
+
+    return 0;
+}
+
+//tc - 0(1)
+//sc - 0(1)
