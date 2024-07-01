cf461e3463dc7ef23e156cfa9c1583cd6b8b9c27
Divyanshu Raj <146466712+divyanshuraj751@users.noreply.github.com>
2024-07-01 23:54:17 +0530

    Update tempCodeRunnerFile.cpp

--- C++CH/tempCodeRunnerFile.cpp
+++ C++CH/tempCodeRunnerFile.cpp
@@ -1,16 +1,2 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-int main() {
-        int n;
-        for(int i=0;i<n;i++) {
-            cout<<i<<" ";
-        }
-        /*cout<<endl;
-        for(int j=n;j>=1;--j) {
-            cout<<j<<" ";
-        }
-        cout<<endl;*/
-    }
-    
 
