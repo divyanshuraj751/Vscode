49e58be58784e0a1231e14ccb6a4488cea024dbe
Divyanshu Raj <146466712+divyanshuraj751@users.noreply.github.com>
2024-07-01 23:52:57 +0530

    Add files via upload

--- C++CH/tempCodeRunnerFile.cpp
+++ C++CH/tempCodeRunnerFile.cpp
@@ -1,34 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-    int t;
-    cin >> t;
-    int n;
-    cin >> n;
-    vector<int> A(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> A[i];
-    }
-    int k;
-    cin >> k;
-
-    sort(A.begin(), A.end());
-    /*for (int i = 0; i < n; i++)
-    {
-        cout << A[i] << " ";
-    }*/
-    cout << endl;
-    int ans=0;
-    for (int i = 0; i < n; i++)
-    {
-        if (A[i] == k)
-        {
-           ans = i;
+int main() {
+        int n;
+        for(int i=0;i<n;i++) {
+            cout<<i<<" ";
         }
-        break;
+        /*cout<<endl;
+        for(int j=n;j>=1;--j) {
+            cout<<j<<" ";
+        }
+        cout<<endl;*/
     }
-    cout<<ans+1<<endl;    
-}
+    
+
